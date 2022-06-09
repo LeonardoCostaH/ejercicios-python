35a9de0ee98c33ebcc21ed4234ef06df42c65756
LeonardoCostaH <105021198+LeonardoCostaH@users.noreply.github.com>
2022-06-09 17:11:18 -0300

    Update ejercicio_7.py

--- funciones/ejercicio_7.py
+++ funciones/ejercicio_7.py
@@ -3,3 +3,7 @@ Escribir una función que reciba una muestra de números en una
 lista y devuelva otra lista con sus cuadrados.
 """
 
+def lista_cuadrados(*args):
+    lista = [i**2 for i in args]
+    return lista
+print(lista_cuadrados(1,2,3,4,5,6,7,8,9))
