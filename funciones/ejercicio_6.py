13f32ea8b59f24d8d3daf25e76361cb9669211f8
LeonardoCostaH <105021198+LeonardoCostaH@users.noreply.github.com>
2022-06-09 17:06:56 -0300

    Update ejercicio_6.py

--- funciones/ejercicio_6.py
+++ funciones/ejercicio_6.py
@@ -3,8 +3,6 @@
 def media_numeros(*args):
     lista = list(args)
     media = lista.__len__()
-    suma = 0
-    for l in lista:
-        suma += l
+    suma = sum(lista)
     return suma / media
 print(media_numeros(1,2,3,4,5))
