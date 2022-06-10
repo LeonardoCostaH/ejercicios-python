42854ff89ff581c2b752b96a45f51b803bdbb855
LeonardoCostaH <105021198+LeonardoCostaH@users.noreply.github.com>
2022-06-10 11:30:40 -0300

    Update ejercicio_9.py

--- funciones/ejercicio_9.py
+++ funciones/ejercicio_9.py
@@ -1,2 +1,12 @@
 """Escribir una función que calcule el máximo común divisor de dos números
 y otra que calcule el mínimo común múltiplo."""
+
+def mcd(n1,n2):
+    resultado_mcd = 0
+    for i in range(1,n1,-1):
+        if i % n1 == 0 and i % n2 == 0:
+            resultado_mdc = i
+            break
+    return resultado_mdc
+
+print(mcd(9,27))
