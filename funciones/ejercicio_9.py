14a35b8c6f3a91ee8679450642de7d32532d3630
LeonardoCostaH <105021198+LeonardoCostaH@users.noreply.github.com>
2022-06-10 18:11:44 -0300

    k

--- funciones/ejercicio_9.py
+++ funciones/ejercicio_9.py
@@ -2,11 +2,23 @@
 y otra que calcule el mínimo común múltiplo."""
 
 def mcd(n1,n2):
-    resultado_mcd = 0
-    for i in range(1,n1,-1):
-        if i % n1 == 0 and i % n2 == 0:
-            resultado_mdc = i
-            break
-    return resultado_mdc
+    rest = 0
+    while (n1 > 0):
+        rest = n1
+        n1 = n2 % n1
+        n2 = rest
+    return n2
+resultado = mcd(9,27)
+print(resultado)
 
-print(mcd(9,27))
+def mcm(n1,n2):
+    if n1 > n2:
+        greater = n1
+    else:
+        greater = n2
+    while (greater % n1 != 0) or (greater % n2 != 0):
+        greater += 1
+    return greater
+
+resultado = mcm(9,27)
+print(resultado)
