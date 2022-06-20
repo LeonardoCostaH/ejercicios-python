26f40d61e67b25d83ef62bf2cae3b4a0f40a476e
LeonardoCostaH <105021198+LeonardoCostaH@users.noreply.github.com>
2022-06-20 15:55:37 -0300

    ejercicio 2 updated

--- listas_tuplas/ejercicio_1.py
+++ listas_tuplas/ejercicio_1.py
@@ -5,9 +5,13 @@ Química, Historia y Lengua) en una lista y la muestre por pantalla."""
 def asignaturas_curso(lista):
     poner_asignatura = input(f"escriba tus asignaturas para almacenarlas: \n")
     lista.append(poner_asignatura)
-    print(lista)
-    
-    
-lista = []
-while True:
-    asignaturas_curso(lista)
+    return poner_asignatura
+
+ 
+ 
+if __name__ == "__main__":
+        
+    lista = []
+    while True:
+        asignaturas_curso(lista)
+        print(lista)
--- listas_tuplas/ejercicio_2.py
+++ listas_tuplas/ejercicio_2.py
@@ -2,3 +2,12 @@
 Química, Historia y Lengua) en una lista y la muestre por pantalla el mensaje Yo estudio <asignatura>, 
 donde <asignatura> es cada una de las asignaturas de la lista."""
 
+import ejercicio_1
+
+def mostrar_lista(lista):
+    for i in lista:
+        print(f"Yo estudio {i}")
+nueva_lista = []
+while True:
+    ejercicio_1.asignaturas_curso(nueva_lista)
+    mostrar_lista(nueva_lista)
