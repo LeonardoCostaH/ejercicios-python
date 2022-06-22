7a465c63ed4ef6461a8fdf500d21a1ea0a0fb0f1
LeonardoCostaH <105021198+LeonardoCostaH@users.noreply.github.com>
2022-06-21 21:01:57 -0300

    Create ejercicio_6.py

---
+++ listas_tuplas/ejercicio_6.py
@@ -0,0 +1,12 @@
+"""Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas,
+Física, Química, Historia y Lengua) en una lista, pregunte al usuario la nota que ha sacado
+en cada asignatura y elimine de la lista las asignaturas aprobadas. Al final el programa debe
+mostrar por pantalla las asignaturas que el usuario tiene que repetir."""
+
+import ejercicio_1
+import ejercicio_3
+
+lista_asignaturas = ["Matemáticas" , "Física", "Química", "Historia", "Lengua"]
+lista_notas = []
+
+ 
