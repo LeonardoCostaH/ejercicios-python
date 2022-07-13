02fb3daf04908f3bcf5b9ce8466e42d869467669
LeonardoCostaH <105021198+LeonardoCostaH@users.noreply.github.com>
2022-07-13 14:26:47 -0300

    Update ejercicio_3.py

--- listas_tuplas/ejercicio_3.py
+++ listas_tuplas/ejercicio_3.py
@@ -3,31 +3,20 @@ Química, Historia y Lengua) en una lista, pregunte al usuario la nota que ha sa
 y después las muestre por pantalla con el mensaje En <asignatura> has sacado <nota> donde <asignatura> 
 es cada una des las asignaturas de la lista y <nota> cada una de las correspondientes notas introducidas 
 por el usuario."""
-import ejercicio_1
-lista_asignaturas = []
-lista_notas = []
-lista_mista = []
-def asignaturas_nota(nota,lista):
-    lista.append(nota)
-    return nota
 
-def mostra_nota(lista_mista, nota_maxima=0):
-    if nota_maxima != 0:
-        for asignatura, nota in lista_mista:
-            if nota <= nota_maxima:
-                print(f"La nota en {asignatura} es {nota}")
-    else:
-        for asignatura, nota in lista_mista:
-            print(f"La nota en {asignatura} es {nota}")
-if __name__ == "__main__":
-    while True:
-        cual_asignatura = input(f"escriba tus asignaturas para almacenarlas: \n")
-        if cual_asignatura == "x":
-            lista_mista = zip(lista_asignaturas,lista_notas)
-            mostra_nota(lista_mista)
-            break
-        else:
-            cual_nota = input(f"Cual es tu nota en {cual_asignatura}: \n")
-            ejercicio_1.asignaturas_curso(cual_asignatura,lista_asignaturas)
-            asignaturas_nota(cual_nota,lista_notas)
-            print(lista_asignaturas,lista_notas)
+
+asignaturas = []
+notas = []
+contador = 0
+
+while True:
+    asignatura = input("Cual asignatura quieres almacenar?\n")
+    if asignatura == "salir":
+        break
+    asignaturas.append(asignatura)
+    nota = input(f"Cual nota has sacado en {asignatura}\n")
+    notas.append(nota)
+    print(f"en {asignaturas[contador]} has sacado {notas[contador]}")
+    contador+=1
+for n, i in enumerate(asignaturas):
+    print(f"En {i} has sacado {notas[n]}")
