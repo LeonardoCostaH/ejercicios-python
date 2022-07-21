46642e1a0d638f38d94d1ac2e479a4a0ab444704
LeonardoCostaH <105021198+LeonardoCostaH@users.noreply.github.com>
2022-07-21 12:36:18 -0300

    Create ejercicio_8.py

---
+++ listas_tuplas/ejercicio_8.py
@@ -0,0 +1,6 @@
+"""Escribir un programa que pida al usuario una palabra y muestre por pantalla si es un palíndromo."""
+
+
+palabra = input("Informe una palabra que sé un palíndromo")
+pali = [i for i in palabra]
+print(pali)
