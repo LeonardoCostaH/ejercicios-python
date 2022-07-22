6e7102e4452c2e74a07e5146b8b9f54addded470
LeonardoCostaH <105021198+LeonardoCostaH@users.noreply.github.com>
2022-07-22 17:13:43 -0300

    Update ejercicio_8.py

--- listas_tuplas/ejercicio_8.py
+++ listas_tuplas/ejercicio_8.py
@@ -1,6 +1,10 @@
 """Escribir un programa que pida al usuario una palabra y muestre por pantalla si es un palíndromo."""
 
-
 palabra = input("Informe una palabra que sé un palíndromo")
 pali = [i for i in palabra]
-print(pali)
+cont = len(pali)
+reve_pali = [i for i in pali.__reversed__()]
+if pali == reve_pali:
+    print(f"La palabra {palabra} es un palíndromo.")
+else:
+    print(f"La palabra {palabra} no es un palíndromo.")
