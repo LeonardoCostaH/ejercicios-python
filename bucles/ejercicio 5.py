3846feb10fb621dc11dfc202d08777a64de999a3
LeonardoCostaH <leonardo.costa@hotmail.com.br>
2022-12-20 17:35:52 -0300

    update ejercicio 5

--- bucles/ejercicio 5.py
+++ bucles/ejercicio 5.py
@@ -2,17 +2,11 @@
 escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual y el número
 de años, y muestre por pantalla el capital obtenido en la inversión cada año que dura la inversión.
 """
-
-inversion = input("informe un valor para invertir: ")
-interes = float(input("informe el interés anual: "))
-anos = input("informa cuantos años dura la inversión: ")
-if not inversion.isdigit() or not anos.isdigit():
-    print("valor informado errado")
-else:
-    inversion = int(inversion)
-    anos = int(anos)
-    i = 1
-    for i in range(anos):
-        inversion = inversion + inversion*interes
-        print(f"el capital obtenido en el año {i} fue de {inversion}")
-
+while True:
+    inversion = int(input("Cuanto queres invertir? "))
+    interes = int(input("Cual sería el interés anual? "))
+    interes = interes * 1.01
+    ano = int(input("por cuantos años querés invertir? "))
+    for i in range(1,ano+1):
+        inversion_anual = ((i*12) * inversion) * interes
+        print(f"tu capital obtenido en el {i}º año de inversión fue {inversion_anual}")
