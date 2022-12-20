49c92078269aebb55286c38ab92036c18e16d00a
LeonardoCostaH <leonardo.costa@hotmail.com.br>
2022-12-20 17:36:49 -0300

    update ejercicio 7

--- bucles/ejercício 7.py
+++ bucles/ejercício 7.py
@@ -2,5 +2,6 @@
 escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10
 """
 
-for i in range(11):
-    print(f"{i} - {i*1},{i*2},{i*3},{i*4},{i*5},{i*6},{i*7},{i*8},{i*9},{i*10}")
+for i in range(1,11):
+    tabla = [j*i for j in range(1,11)]
+    print(tabla)
