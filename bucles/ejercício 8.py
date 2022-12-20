2d3114e235b4f7cbac2c044a502493d20250d9c4
LeonardoCostaH <leonardo.costa@hotmail.com.br>
2022-12-20 17:37:16 -0300

    update ejercicio 8

--- bucles/ejercício 8.py
+++ bucles/ejercício 8.py
@@ -2,8 +2,15 @@
 Escribir un programa que pida al usuario un número entero y muestre por pantalla
 un triángulo rectágulo como el de más abajo.
 """
-numero_entero = int(input("informe un número entero: "))
-for i in range(1, numero_entero+1, 2):
-    for j in range(i, 0, -2):
-        print(j, end=" ")
-    print("")
+while True:
+    entero = input("informe un número entero: ")
+    if not entero.isdigit():
+        print("ese no es un numero entero.")
+        continue
+    else:
+        entero = int(entero)
+        for i in range(1,entero+1,2):
+            for j in range(i,0,-2):
+                print(j,end=" ")
+            print()
+
