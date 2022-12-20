76c1293d212f9570c747a4b015d55763e1dfeb19
LeonardoCostaH <leonardo.costa@hotmail.com.br>
2022-12-20 17:34:18 -0300

    update ejercicio 4

--- bucles/ejercicio 4.py
+++ bucles/ejercicio 4.py
@@ -2,10 +2,14 @@
 escribir un programa que pida al usuario un número entero positivo y muestre por
 pantalla la cuenta atrás desde ese número hasta cero separados por coma.
 """
-numero_entero = input("informe un número entero positivo: ")
-if not numero_entero.isdigit():
-    print("número inválido.")
-else:
-    numero_entero = int(numero_entero)
-    for i in range(numero_entero,0,-1):
-        print(f"{i}",end=",")
+while True:
+    entero = input("escriba un numero entero: ")
+    if not entero.isdigit():
+        print("por favor, el numero tiene que ser un entero! ")
+    else:
+        entero = int(entero)
+        # for i in range(entero,-1,-1):
+        #     print(i,end=",")
+
+        regresivo = [i for i in range(entero,-1,-1)]
+        print(regresivo)
