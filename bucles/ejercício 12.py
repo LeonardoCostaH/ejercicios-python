6b0383c654e3210af5d4db3ba4036636a1e15708
LeonardoCostaH <leonardo.costa@hotmail.com.br>
2022-12-21 11:36:43 -0300

    update ejercício 12.py

--- bucles/ejercício 12.py
+++ bucles/ejercício 12.py
@@ -3,18 +3,23 @@ escribir un programa en el que se pregunte al usuario por una frase y una letra,
 y muestre por pantalla el número de veces que aparece la letra en la frase.
 """
 
-"""frase = input("escriba una frase: ")
-letra = input("ahora una letra: ")
-contador = 0
-for i in range(len(frase)):
-    if letra == frase[i]:
-        contador+=1
-print(contador)"""
 
-frase = input("introduce una frase: ")
-letra = input("introduce una letra")
-contador = 0
-for i in frase:
-    if i == letra:
-        contador+=1
-print(contador)
+# while True:
+#     frase = input("escriba una frase: \n")
+#     letra = input("escriba una letra: \n")
+#     count = 0
+#     for i in range(len(frase)):
+#         if letra == frase[i]:
+#             count+= 1
+#     print(f"la frase tiene la letra {letra} {count} veces.")
+
+
+# while True:
+#     frase = input("escriba una frase: \n")
+#     letra = input("ahora una letra: \n")
+#     count = 0
+#     for i in frase:
+#         if i == letra:
+#             count+=1
+#     print(f"la frase tiene la letra {letra} {count} veces.")
+
