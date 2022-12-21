4052b53ef9ed795c1ebb6fcdd433a8f0a14ff4f3
LeonardoCostaH <leonardo.costa@hotmail.com.br>
2022-12-20 22:44:32 -0300

    update ejercicios-python.iml

--- bucles/ejercício 10.py
+++ bucles/ejercício 10.py
@@ -2,4 +2,16 @@
 escribir un programa que pida al usuario un número entero y muestre por pantalla si es
 un número primo o no.
 """
-numero_entero = ("informa un número entero: ")
+while True:
+    entero = input("escriba un numero entero: ")
+    if not entero.isdigit():
+        print(f"{entero} no es un numero entero.")
+    else:
+        entero = int(entero)
+        for i in range(2,entero):
+            if entero % i == 0:
+                print(f"{entero} no es un numero primo.")
+                break
+            elif i == entero-1:
+                if not entero % i == 0:
+                    print(f"{entero} es un numero primo")
