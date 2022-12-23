99f452609c8648f029b7b8f195f133fbfa724e42
LeonardoCostaH <leonardo.costa@hotmail.com.br>
2022-12-23 11:10:48 -0300

    update ejercicio_2.py de listas y tuplas

--- listas_tuplas/ejercicio_2.py
+++ listas_tuplas/ejercicio_2.py
@@ -2,10 +2,18 @@
 Química, Historia y Lengua) en una lista y la muestre por pantalla el mensaje Yo estudio <asignatura>, 
 donde <asignatura> es cada una de las asignaturas de la lista."""
 
-lista = []
-contador = 0
+asignaturas = []
 while True:
-    asignatura = input("Cual asignatura quieres almacenar?\n")
-    lista.append(asignatura)
-    print(f"Yo estudio {lista[contador]}")
-    contador += 1
+    asignatura = input("Cual asignatura quieres almacenar? \n")
+    asignaturas.append(asignatura)
+    for i in asignaturas:
+        print(f"Yo estudio {i}")
+
+#
+# lista = []
+# contador = 0
+# while True:
+#     asignatura = input("Cual asignatura quieres almacenar?\n")
+#     lista.append(asignatura)
+#     print(f"Yo estudio {lista[contador]}")
+#     contador += 1
