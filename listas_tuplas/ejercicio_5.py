5126c3e44f0cd976f2a3c242e69f923b3afe74bc
LeonardoCostaH <leonardo.costa@hotmail.com.br>
2022-12-23 12:39:27 -0300

    update ejercicio_5.py de listas y tuplas

--- listas_tuplas/ejercicio_5.py
+++ listas_tuplas/ejercicio_5.py
@@ -1,7 +1,10 @@
 """Escribir un programa que almacene en una lista los números del 1 al 10 y los muestre por
 pantalla en orden inverso separados por comas."""
 
-lista = [i for i in range(1,11)]
+# lista = [i for i in range(1,11)]
+# lista.reverse()
+# print(lista)
+
+lista = list(range(1,11))
 lista.reverse()
 print(lista)
-
