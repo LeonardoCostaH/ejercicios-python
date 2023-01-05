d2c797337b0cd6a19dc26316e4354a4597a46ddc
LeonardoCostaH <leonardo.costa@hotmail.com.br>
2023-01-05 16:49:17 -0300

    update ejercicio_6.py de listas y tuplas

--- listas_tuplas/ejercicio_6.py
+++ listas_tuplas/ejercicio_6.py
@@ -4,23 +4,40 @@ en cada asignatura y elimine de la lista las asignaturas aprobadas. Al final el 
 mostrar por pantalla las asignaturas que el usuario tiene que repetir."""
 
 
-lista_asignaturas = []
-lista_notas = []
-lista_mista = []
-
-
-
+asignaturas = []
+notas = []
 while True:
-    asignatura = input(f"almacente aquí tus asignaturas: \n")
-    if asignatura == "x":
-        lista_mista = zip(lista_asignaturas,lista_notas)
+    asignatura = input("almacene aquí tus asignaturas o 'salir' para terminar: \n ")
+    if asignatura != "salir":
+        nota = int(input(f"informe cual es tu nota en {asignatura}: \n"))
+        if nota < 7:
+            asignaturas.append(asignatura)
+            notas.append(nota)
+        else:
+            continue
+    else:
+        for i, j in enumerate(asignaturas):
+            print(f"tenes que repetir en {j} ya que sacaste un {notas[i]}")
         break
-    else:
-        lista_asignaturas.append(asignatura)
-        nota = int(input(f"Cual es tu nota en {asignatura}: \n"))
-        lista_notas.append(nota)
-for i in lista_mista:
-    if i[1] >= 7:
-        continue
-    else:
-        print(f"tienes que repetir en {i[0]}")
+
+
+
+#
+# lista_asignaturas = []
+# lista_notas = []
+# lista_mista = []
+#
+# while True:
+#     asignatura = input(f"almacente aquí tus asignaturas o 'x' para terminar: \n")
+#     if asignatura == "x":
+#         lista_mista = zip(lista_asignaturas,lista_notas)
+#         break
+#     else:
+#         lista_asignaturas.append(asignatura)
+#         nota = int(input(f"Cual es tu nota en {asignatura}: \n"))
+#         lista_notas.append(nota)
+# for i in lista_mista:
+#     if i[1] >= 7:
+#         continue
+#     else:
+#         print(f"tienes que repetir en {i[0]}")
