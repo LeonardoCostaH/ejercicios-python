348d1ef608f6c0e82fb57a9edc530f4ebb02a39b
Leonardo Costa <105021198+LeonardoCostaH@users.noreply.github.com>
2023-07-11 17:37:58 -0300

    Create ej3.py

---
+++ pandas/ej3.py
@@ -0,0 +1,6 @@
+"""Escribir una función que reciba un diccionario con las notas de los alumnos de un curso y 
+devuelva una serie con las notas de los alumnos aprobados ordenadas de mayor a menor."""
+
+import pandas as pd
+
+class notas_alumnos:
