cd0892817f91492e5d2ce4f11e13f973f6dff32b
Leonardo Costa <105021198+LeonardoCostaH@users.noreply.github.com>
2023-07-12 17:41:58 -0300

    update ej3

--- pandas/ej3.py
+++ pandas/ej3.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 class NotasAlumnos:
     def __init__(self):
-        dic_notas = {}
+        self.__dic_notas = {}
     def informar_notas(self):
         while True:
             alumno = input("informe un alumno: (o 'exit' para salir) \n")
@@ -13,13 +13,14 @@ class NotasAlumnos:
                 break
             else:
                 nota = float(input(f"Cuál es la nota de {alumno}?\n"))
-                self.dic_notas[alumno] = nota
+                self.__dic_notas[alumno] = nota
     def mostrar_notas(self):
-        serie = pd.Series(self.dic_notas)
+        serie = pd.Series(self.__dic_notas)
         return serie
     def aprobados(self):
-        self.mostrar_notas()[mostrar_notas >= 5].sort_values(ascending=False)
-        return nota_ord
+        notas = pd.Series(self.__dic_notas)
+        return notas[notas >= 5].sort_values(ascending=False)
+        
 
 nota = NotasAlumnos()
 nota.informar_notas()
